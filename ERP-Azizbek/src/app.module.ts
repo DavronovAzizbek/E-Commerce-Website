6d38c343021223ad138b0d66cf1a3eee1603d222
githubusername <azizbekdavronov1110@gmail.com>
2024-12-12 11:15:00 +0500

    Profile

---
+++ ERP-Azizbek/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Courses } from './courses/entities/course.entity';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { AuthModule } from './auth/auth.module';
+import { Auth } from './auth/entities/auth.entity';
+import { ProfileModule } from './profile/profile.module';
+import { User } from './users/entities/user.entity';
+import { Profile } from './profile/entities/profile.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'azizbek002',
+      database: 'qwe',
+      entities: [User, Courses, Auth,Profile],
+      synchronize: true,
+    }),
+    CoursesModule,
+    UsersModule,
+    AuthModule,
+    ProfileModule,
+  ],
+})
+export class AppModule {}
---
+++ ERP-Azizbek/src/courses/entities/course.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Courses {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+  users: any;
+}
