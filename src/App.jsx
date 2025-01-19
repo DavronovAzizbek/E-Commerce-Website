7982c31dfe4f745a027f10897c2ab2dabcb0a5c1
githubusername <azizbekdavronov1110@gmail.com>
2025-01-19 23:32:03 +0500

    Let's start with the title part

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-[50px] bg-slate-500">Welcome back</h1>
+      <Header/>
     </>
   );
 }
