e36cd438cbe3c9749280931f9bd0e4294faeaa66
githubusername <azizbekdavronov1110@gmail.com>
2025-01-20 00:37:04 +0500

    Order Tracking added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,20 +4,33 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <div className="top-strip py-2">
+      <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]">
         <div className="container">
           <div className="flex items-center justify-between">
             <div className="col1 w-[50%]">
-              <p className="text-[14px] font-[500]">
+              <p className="text-[12px] font-[500]">
                 Yangi mavsum uslublariga 50% gacha chegirmaga ega boling, faqat
                 cheklangan vatq
               </p>
             </div>
 
             <div className="col2 flex items-center justify-end">
-              <ul>
+              <ul className="flex items-center gap-3">
                 <li className="list-none">
-                  <Link to="help-center">Yordam markazi</Link>
+                  <Link
+                    to="/help-center"
+                    className="text-[13px] link font-[500] transition"
+                  >
+                    Yordam markazi{""}
+                  </Link>
+                </li>
+                <li className="list-none">
+                  <Link
+                    to="/order-tracking"
+                    className="text-[13px] link font-[500] transition"
+                  >
+                    Buyurtmani kuzatish
+                  </Link>
                 </li>
               </ul>
             </div>
