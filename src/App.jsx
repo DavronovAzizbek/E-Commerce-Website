866778c9cd8e60a0346f1c6ed713dfb240bb11be
githubusername <azizbekdavronov1110@gmail.com>
2025-01-19 23:17:11 +0500

    required packages are installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-[50px]">Welcome back</h1>
+      <h1 className="text-[50px] bg-slate-500">Welcome back</h1>
     </>
   );
 }
