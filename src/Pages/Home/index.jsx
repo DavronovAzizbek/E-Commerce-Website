c206601177b046774b888692ed895db512ee258b
githubusername <azizbekdavronov1110@gmail.com>
2025-01-26 22:18:25 +0500

    AdsBannerSlider in the part changes added and updated and BannerBox part added

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import HomeSlider from "../../components/HomeSlider";
 import HomeCatSlider from "../../components/HomeCatSlider";
 import { LiaShippingFastSolid } from "react-icons/lia";
+import AdsBannerSlider from "../../components/AdsBannerSlider";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
 
       <section className="py-16 bg-white">
         <div className="container">
-          <div className="freeShipping w-full py-4 p-4 border-2 border-[#ff5252] flex items-center justify-between rounded-md">
+          <div className="freeShipping w-full py-4 p-4 border-2 border-[#ff5252] flex items-center justify-between rounded-md mb-7">
             <div className="col1 flex items-center gap-4">
               <LiaShippingFastSolid className="text-[50px]" />
               <span className="text-[20px] font-[600] uppercase">
@@ -26,6 +27,8 @@ const Home = () => {
 
             <p className="font-bold text-[25px]">- Faqat $200*</p>
           </div>
+
+          <AdsBannerSlider items={4} />
         </div>
       </section>
 
--- src/components/AdsBannerSlider/index.jsx
+++ src/components/AdsBannerSlider/index.jsx
@@ -1,7 +1,43 @@
-const AdsBannerSlider = () => {
-    return (
-        <div>index</div>
-    )
-}
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+import BannerBox from "../BannerBox";
+
+const AdsBannerSlider = (props) => {
+  return (
+    <div className="py-5 w-full">
+      <Swiper
+        // eslint-disable-next-line react/prop-types
+        slidesPerView={props.items}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation]}
+        className="smlBtn"
+      >
+        <SwiperSlide>
+          <BannerBox img={"/Banner1.webp"} link={"/"} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BannerBox img={"/Banner2.jpg"} link={"/"} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BannerBox img={"/Banner3.png"} link={"/"} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BannerBox img={"/Banner4.jpg"} link={"/"} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <BannerBox img={"/Banner5.png"} link={"/"} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
 export default AdsBannerSlider;
