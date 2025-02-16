16cc6bb22859e2b104e770de3a469bb4a34281e6
githubusername <azizbekdavronov1110@gmail.com>
2025-02-16 22:22:43 +0500

    Added Size and other items to the ProductDetails section

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -2,8 +2,12 @@ import Link from "@mui/material/Link";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import ProductZoom from "../../components/ProductZoom";
 import Rating from "@mui/material/Rating";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 const ProductDetails = () => {
+  const [productActionIndex, setProductActionIndex] = useState(null);
+
   return (
     <>
       <div className="py-5">
@@ -44,7 +48,7 @@ const ProductDetails = () => {
           </div>
 
           <div className="productContent w-[60%]">
-            <h1 className="text-[22px] font-[600] mb-2">
+            <h1 className="text-[24px] font-[600] mb-2">
               Cropped Satin Bomber Jacket
             </h1>
             <div className="flex items-center gap-3">
@@ -65,21 +69,64 @@ const ProductDetails = () => {
             </div>
 
             <div className="flex items-center gap-4 mt-4">
-              <span className="oldPrice line-through text-gray-500 text-[18px] font-[500]">
+              <span className="oldPrice line-through text-gray-500 text-[20px] font-[500]">
                 $110.00
               </span>
-              <span className="price text-primary text-[18px] font-[600]">
+              <span className="price text-primary text-[20px] font-[600]">
                 $94.00
+              </span>
+
+              <span className="text-[14px]">
+                Available In Stock:{" "}
+                <span className="text-green-600 text-[14px] font-bold">
+                  147 Items
+                </span>
               </span>
             </div>
 
-            <br />
-
-            <p>
+            <p className="mt-3 pr-10 mb-5">
               Established fact that a reader will be distracted by the readable
               content of a page when looking at its layout. The point of using
               Lorem Ipsum is that it has a more-or-less.
             </p>
+
+            <div className="flex items-center gap-3">
+              <span className="text-[16px]">Size:</span>
+              <div className="flex items-center gap-1 actions">
+                <Button
+                  className={`${
+                    productActionIndex === 0 ? "!bg-primary !text-white" : ""
+                  }`}
+                  onClick={() => setProductActionIndex(0)}
+                >
+                  S
+                </Button>
+                <Button
+                  className={`${
+                    productActionIndex === 1 ? "!bg-primary !text-white" : ""
+                  }`}
+                  onClick={() => setProductActionIndex(1)}
+                >
+                  M
+                </Button>
+                <Button
+                  className={`${
+                    productActionIndex === 2 ? "!bg-primary !text-white" : ""
+                  }`}
+                  onClick={() => setProductActionIndex(2)}
+                >
+                  L
+                </Button>
+                <Button
+                  className={`${
+                    productActionIndex === 3 ? "!bg-primary !text-white" : ""
+                  }`}
+                  onClick={() => setProductActionIndex(3)}
+                >
+                  XL
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </section>
