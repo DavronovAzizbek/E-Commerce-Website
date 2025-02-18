747a4f66c9e48d6ca686d7501e719ee2c90a3c04
githubusername <azizbekdavronov1110@gmail.com>
2025-02-19 00:01:21 +0500

    ProductDetails section added

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,80 @@ import Home from "./Pages/Home";
 import ProductListing from "./Pages/ProductListing";
 import Footer from "./components/Footer";
 import ProductDetails from "./Pages/ProductDetails";
+import { createContext } from "react";
+import { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import ProductZoom from "./components/ProductZoom";
+
+const MyContext = createContext();
 
 function App() {
+  const [openProductDetailsModal, setOpenProductDetailsModal] = useState(true);
+  const [maxWidth, setMaxWidth] = useState("lg");
+  const [fullWidth, setFullWidth] = useState(true);
+
+  const handleClickOpenProductDetailsModal = () => {
+    setOpenProductDetailsModal(true);
+  };
+
+  const handleCloseProductDetailsModal = () => {
+    setOpenProductDetailsModal(false);
+  };
+
+  const values = {};
+
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path={"/"} exact={true} element={<Home />} />
-          <Route
-            path={"/productListing"}
-            exact={true}
-            element={<ProductListing />}
-          />
-          <Route
-            path={"/product/:id"}
-            exact={true}
-            element={<ProductDetails />}
-          />
-        </Routes>
-        <Footer />
+        <MyContext.Provider value={values}>
+          <Header />
+          <Routes>
+            <Route path={"/"} exact={true} element={<Home />} />
+            <Route
+              path={"/productListing"}
+              exact={true}
+              element={<ProductListing />}
+            />
+            <Route
+              path={"/product/:id"}
+              exact={true}
+              element={<ProductDetails />}
+            />
+          </Routes>
+          <Footer />
+        </MyContext.Provider>
       </BrowserRouter>
+
+      <Dialog
+        open={openProductDetailsModal}
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}
+        onClose={handleCloseProductDetailsModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="productDetailsModal"
+      >
+        <DialogContent>
+          <div className="flex items-center w-full productDetailsModalContainer relative">
+            <Button
+              className="!w-[40px] !h-[40px] !min-w-[40px] !rounded-full !text-[#000] !absolute top-[0px] right-[0px]"
+              onClick={handleCloseProductDetailsModal}
+            >
+              <IoCloseSharp />
+            </Button>
+            <div className="col1 w-[40%]">
+              <ProductZoom />
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
